d269778b893613a3ff2610acd5ad3bd920fbd358
Sanjar2010 <sanzarkemelov432@gmail.com>
2023-04-15 14:49:05 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+//nomer 1
+// function getNum() {
+//     const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let num = 5
+//      console.log(num * 10);
+//     }, 3000)
+//   })
+// }
+// getNum()
+
+
+
+//nomer 2
+// function getNum() {
+//     const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+        
+//         let num1 = 5
+//         let num2 = 10
+//      console.log(num1 + num2);
+//     }, 5000)
+//   })
+// }
+// getNum()
+
+
+
+
+//nomer3
+// function getNum() {
+//     const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+        
+//   const arr = [2,1,3,4,5]
+//   const array  =   arr.sort() 
+
+//   console.log(array[0]);
+
+//     }, 2000)
+//   })
+// }
+// getNum()
+
+
+
+//nomer 4
+// function getNum() {
+//     const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+        
+//         let num1 = 5
+//         let num2 = 10
+//      console.log(num1 + num2);
+//     }, 5000)
+//   })
+// }
+// getNum()
